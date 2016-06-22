3e03714bcffceb1d2b1ff1db9768704fa44386fc
Udit Manektala <code@getbraintree.com>
2016-06-21 20:48:47 -0700

    Fixing Routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactRouter = require('react-router');
-var Home = require('./components/home');
-var Main = require('./components/main');
+var Home = require('../components/home');
+var Main = require('../components/main');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
@@ -10,8 +10,7 @@ var hashHistory = ReactRouter.hashHistory;
 var Routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      <IndexRoute component={Home}j
+      <IndexRoute component={Home} />
     </Route>
   </Router>
-
 )
