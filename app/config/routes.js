2e6c61749e436b78a9ca76fa4bdb9bfe89bd36e6
Udit Manektala <code@getbraintree.com>
2016-06-28 20:37:38 -0700

    Adding a new route for the weather details page

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var HomeContainer = require('../containers/HomeContainer');
 var WeatherContainer = require('../containers/WeatherContainer');
+var WeatherDetailsContainer = require('../containers/WeatherDetailsContainer');
 var MainComponent = require('../components/main');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
@@ -13,6 +14,7 @@ var Routes = (
     <Route path='/' component={MainComponent}>
       <IndexRoute component={HomeContainer} />
       <Route path='/weather/:area' component={WeatherContainer} />
+      <Route path='/weather/:area/:day' component={WeatherDetailsContainer} />
     </Route>
   </Router>
 )
---
+++ app/containers/WeatherDetailsContainer.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+
+var WeatherDetailsContainer = React.createClass({
+
+  render: function(){
+    return (
+      <div>Hello World</div>
+    )
+  }
+})
+
+module.exports = WeatherDetailsContainer;
