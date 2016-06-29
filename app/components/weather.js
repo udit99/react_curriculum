1143b217c807762e9f62415d59bbd79fe091efc3
Udit Manektala <code@getbraintree.com>
2016-06-28 19:50:36 -0700

    Factored out weather from weather collection

--- app/components/weather.js
+++ app/components/weather.js
@@ -1,9 +1,11 @@
 var React = require('react')
 
 var Weather = function(props){
-  return (
-    <div> Hello Worldz </div>
-  )
+    return <tr>
+             <td>{props.date}</td>
+             <td>{props.temperature}</td>
+             <td>{props.description}</td>
+           </tr>
 }
 
 module.exports = Weather;
--- app/components/weather_collection.js
+++ app/components/weather_collection.js
@@ -1,13 +1,12 @@
 var React = require('react')
+var Weather = require('./weather')
 
 var WeatherCollection = function(props){
   weather_record_tds = props.weatherInfo.map(function(weatherRecord){
-    debugger;
-    return <tr>
-             <td>{weatherRecord['dt_txt']}</td>
-             <td>{(Math.round((weatherRecord['main']['temp'] * 1.8 - 459.67)*100))/100}</td>
-             <td>{weatherRecord['weather'][0]['description']}</td>
-           </tr>
+    return <Weather
+            date={weatherRecord['dt_txt']}
+            temperature={(Math.round((weatherRecord['main']['temp'] * 1.8 - 459.67)*100))/100}
+            description={weatherRecord['weather'][0]['description']} />
   })
   return (
     <table>
